4081d5a9e89db27ba0ff4c9abf1c6413b7660d20
alagache <alagache@student.42.fr>
2020-06-07 10:44:40 +0200

    change toto.s to have all parmeter type for every op

--- asm/toto.s
+++ asm/toto.s
@@ -1,11 +1,52 @@
- 	 	
-.name "zork"
- 	 	 
-.comment "I'M ALIVE"
+.name "toto"
+.comment "BEST TEST FILE FOR ASM"
  	
-l2:		sti r99,  1, %2147483648
-		live %2147483647
-
-live:	live %1
-		zjmp %:live		
-
+live %42
+ld %42, r1
+ld 41, r2
+st r2, 42
+st r2, r1
+add r1, r2, r3
+sub r1, r2, r3
+and r1 , r2, r3
+and 1 , 2, r3
+and r1 , 2, r3
+and 1 , r2, r3
+and r1 , %2, r3
+and %1 , r2, r3
+or r1 , r2, r3
+or 1 , 2, r3
+or r1 , 2, r3
+or 1 , r2, r3
+or r1 , %2, r3
+or %1 , r2, r3
+xor r1 , r2, r3
+xor 1 , 2, r3
+xor r1 , 2, r3
+xor 1 , r2, r3
+xor r1 , %2, r3
+xor %1 , r2, r3
+zjmp %42
+ldi r1, r2, r3
+ldi r1, %2, r3
+ldi %1, r2, r3
+ldi %1, %2, r3
+ldi 1, r2, r3
+ldi 1, %2, r3
+sti r1, r2, r3
+sti r1, r2, %3
+sti r1, 2, r3
+sti r1, 2, %3
+sti r1, %2, r3
+sti r1, %2, %3
+fork %42
+lld 1, r1
+lld %1, r1
+lldi r1, r2, r3
+lldi r1, %2, r3
+lldi 1, r2, r3
+lldi 1, %2, r3
+lldi %1, r2, r3
+lldi %1, %2, r3
+lfork %42
+aff r1
